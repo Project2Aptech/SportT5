0afeeaa442cb1764e31cb1d3caa91c67d8c5f674
KaiSser <tuanhiep29797@gmail.com>
2026-05-17 09:55:54 +0700

    update

--- src/main/java/com/sportt5/App.java
+++ src/main/java/com/sportt5/App.java
@@ -1,4 +1,25 @@
-package org.example.app;
+package com.sportt5;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        URL fxml = getClass().getResource("/com.sportt5/view/HomeView.fxml");
+        FXMLLoader loader = new FXMLLoader(fxml);
+        Scene scene = new Scene(loader.load(), 1040, 870);
+        stage.setTitle("SportT5");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
