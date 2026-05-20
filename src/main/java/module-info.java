f170678a1c8c07eb491b72654713e0ba38eacfb1
KaiSser <tuanhiep29797@gmail.com>
2026-05-20 22:08:29 +0700

    home_dao and library_dao

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,4 +13,5 @@ module com.sportt5 {
     opens com.sportt5 to javafx.fxml;
     opens com.sportt5.controller to javafx.fxml;
     exports com.sportt5;
+    opens com.sportt5.controller.home to javafx.fxml;
 }
