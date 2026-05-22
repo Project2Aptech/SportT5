27e2453f34aec79d78cd04fe994b41c66687f724
KaiSser <tuanhiep29797@gmail.com>
2026-05-22 14:31:59 +0700

    test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,17 +2,17 @@ module com.sportt5 {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.media;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
-    requires com.zaxxer.hikari;
     requires java.sql;
+    requires com.zaxxer.hikari;
 
     opens com.sportt5 to javafx.fxml;
-    opens com.sportt5.controller to javafx.fxml;
-
-    exports com.sportt5;
     opens com.sportt5.controller.home to javafx.fxml;
+    opens com.sportt5.controller.library to javafx.fxml;
+    exports com.sportt5;
 }
