14b0cdefbae8e9f9854db61870da31feae4d9b5e
Daisme12 <manhnhat1204@gmail.com>
2026-05-27 03:49:31 +0700

    done logic register

--- src/main/java/com/sportt5/service/AuthService.java
+++ src/main/java/com/sportt5/service/AuthService.java
@@ -9,12 +9,38 @@ import java.io.IOException;
 import java.net.http.HttpResponse;
 
 public class AuthService {
-    private final String endpoint = "auth/login";
+    private final String endpointLogin = "auth/login";
+    private final String endpointRegister = "auth/register";
     private ObjectMapper mapper = new ObjectMapper();
+
+    public AuthResponse register(String nameInput,String emailInput, String passwordInput) throws IOException, InterruptedException {
+        String jsonPayload = String.format("""
+            {
+                "username":"%s",
+                "email":"%s",
+                "password":"%s"
+            }
+            """, nameInput, emailInput, passwordInput);
+
+        HttpResponse<String> response = ConnectionServer.post(endpointRegister, jsonPayload);
+        JsonNode node = mapper.readTree(response.body());
+
+        if(response.statusCode() == 200){
+            String token = node.get("accessToken").asText();
+            JsonNode userNode = node.get("user");
+            Users user = mapper.treeToValue(userNode, Users.class);
+
+            return new AuthResponse(token, user);
+        }
+        else {
+            String message = node.get("message").asText();
+            throw new RuntimeException(message);
+        }
+    }
 
     public AuthResponse authenticate(String login,String password) throws IOException, InterruptedException {
         String jsonPayload = String.format("{\"email\":\"%s\", \"password\":\"%s\"}", login, password);
-        HttpResponse<String> response = ConnectionServer.post(endpoint, jsonPayload);
+        HttpResponse<String> response = ConnectionServer.post(endpointLogin, jsonPayload);
         JsonNode node = mapper.readTree(response.body());
 
         if (response.statusCode() == 200) {
